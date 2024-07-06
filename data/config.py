88179a35d94a7ddd37300428187b41d4b95cba5e
DatPill <smartcrackerprezents@gmail.com>
2024-07-06 11:18:34 +0300

    first commit

---
+++ data/config.py
@@ -0,0 +1,10 @@
+from typing import Final
+
+from dotenv import dotenv_values
+
+
+config = dotenv_values()
+
+SESSION: Final[str] = str(config['SESSION'])
+API_ID: Final[str] = str(config['API_ID'])
+API_HASH: Final[str] = str(config['API_HASH'])
